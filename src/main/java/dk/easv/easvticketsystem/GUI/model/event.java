be09c908102fd3d08e369f4e5dcb4159050fec49
Elena Borgwardt Vahlun <Elena.Borgwardt@hotmail.com>
2026-03-30 15:34:42 +0200

    Represents an Event

--- src/main/java/dk/easv/easvticketsystem/GUI/model/event.java
+++ src/main/java/dk/easv/easvticketsystem/GUI/model/event.java
@@ -1,9 +1,18 @@
 package dk.easv.easvticketsystem.GUI.model;
 
-public class event {
+//Represents an Event
+public class Event {
     private String title;
     private String date;
     private String time;
     private String location;
-    private String maxTickets;
+    private int maxTickets;
+
+    public Event(String title, String date, String time, String location, int maxTickets) {
+        this.title = title;
+        this.date = date;
+        this.time = time;
+        this.location = location;
+        this.maxTickets = maxTickets;
+    }
 }
