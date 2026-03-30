eb1000206d200b791c4de229a9263fc49f3102f0
Elena Borgwardt Vahlun <Elena.Borgwardt@hotmail.com>
2026-03-30 15:18:35 +0200

    model + event.java

---
+++ src/main/java/dk/easv/easvticketsystem/GUI/model/event.java
@@ -0,0 +1,9 @@
+package dk.easv.easvticketsystem.GUI.model;
+
+public class event {
+    private String title;
+    private String date;
+    private String time;
+    private String location;
+    private String maxTickets;
+}
